ae9cabb4bd7e061d16884c96734ee6a8fd931989
Gianfrano Giordano <gianfranogiordano@gmail.com>
2026-04-12 18:29:59 -0400

    fix: add firebase + vapidKey to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,15 @@
 export const environment = {
   production: true,
-  // Esta URL la reemplazarás en el AWS Amplify Build Settings 
-  // o se quemará aquí si ya tienes el dominio de la API.
   apiUrl: 'https://api.un2x3.store',
-  socketUrl: 'https://api.un2x3.store'
+  socketUrl: 'https://api.un2x3.store',
+  firebase: {
+    apiKey: 'AIzaSyAwfgfNdAUR_IMRymLAbAuYLRCxNPsW2jo',
+    authDomain: 'un-2x3.firebaseapp.com',
+    projectId: 'un-2x3',
+    storageBucket: 'un-2x3.firebasestorage.app',
+    messagingSenderId: '97741611493',
+    appId: '1:97741611493:web:3417b7d5b6b21d861acf0c',
+    measurementId: 'G-J7M08J08DP',
+  },
+  vapidKey: 'BAknoVyCyedu2GLax9jpIAollmALF1D2LL0isqZt5XDB78v2PybPd4htObhVCmyUe8JM4GzxORJCLeJabua4Keg',
 };
